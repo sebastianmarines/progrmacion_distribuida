a80d6a96c054a9725d0e4b7cfcb97adf73c903ef
Sebastian Marines <a01383056@tec.mx>
2023-06-11 16:46:59 -0600

    Registrar productos

--- cliente_tienda.erl
+++ cliente_tienda.erl
@@ -1,5 +1,6 @@
 -module(cliente_tienda).
--export([suscribe/1, unsuscribe/1]).
+-export([suscribe/1, unsuscribe/1, lista_existencias/0]).
+-compile(common).
 -import(common, [llama_tienda/1]).
 
 suscribe(Cliente) ->
@@ -7,3 +8,6 @@ suscribe(Cliente) ->
 
 unsuscribe(Cliente) ->
     llama_tienda({eliminar_suscripcion, Cliente}).
+
+lista_existencias() ->
+    llama_tienda(lista_productos).
--- servidor_tienda.erl
+++ servidor_tienda.erl
@@ -1,5 +1,14 @@
 -module(servidor_tienda).
--export([abre_tienda/0, servidor/1, cierra_tienda/0, lista_socios/0, probar/0]).
+-export([
+    abre_tienda/0,
+    servidor/1,
+    cierra_tienda/0,
+    lista_socios/0,
+    probar/0,
+    servidor_producto/1,
+    registra_producto/2
+]).
+-compile(common).
 -import(common, [llama_tienda/1]).
 
 servidor(Datos) ->
@@ -13,7 +22,7 @@ servidor(Datos) ->
                     servidor(Datos);
                 false ->
                     De ! {servidor_tienda, ok},
-                    servidor({[Quien | element(1, Datos)], element(2, Datos)})
+                    servidor({[Quien | element(1, Datos)], element(2, Datos), element(3, Datos)})
             end,
             De ! {servidor_tienda, ok},
             servidor(Datos);
@@ -27,13 +36,31 @@ servidor(Datos) ->
                     servidor(Datos);
                 true ->
                     De ! {servidor_tienda, ok},
-                    servidor({lists:delete(Quien, element(1, Datos)), element(2, Datos)})
+                    servidor({
+                        lists:delete(Quien, element(1, Datos)), element(2, Datos), element(3, Datos)
+                    })
             end,
             De ! {servidor_tienda, ok},
             servidor(Datos);
         {De, lista_socios} ->
             io:format("Listando socios~n"),
             De ! {servidor_tienda, element(1, Datos)},
+            servidor(Datos);
+        {De, {registra_producto, Producto, Cantidad}} ->
+            io:format("Registrando producto ~p~n", [Producto]),
+            De ! {servidor_tienda, ok},
+            servidor(
+                {
+                    element(1, Datos),
+                    [
+                        {Producto, crear_servidor_producto(Cantidad)} | element(2, Datos)
+                    ],
+                    element(3, Datos)
+                }
+            );
+        {De, lista_productos} ->
+            io:format("Listando productos~n"),
+            De ! {servidor_tienda, element(2, Datos)},
             servidor(Datos);
         {De, _} ->
             De ! {servidor_tienda, {error, "Mensaje no reconocido"}},
@@ -43,7 +70,7 @@ servidor(Datos) ->
 abre_tienda() ->
     register(
         servidor_tienda,
-        spawn(servidor_tienda, servidor, [{[], []}])
+        spawn(servidor_tienda, servidor, [{[], [], 0}])
     ).
 
 cierra_tienda() ->
@@ -52,11 +79,40 @@ cierra_tienda() ->
 lista_socios() ->
     llama_tienda(lista_socios).
 
+registra_producto(Producto, Cantidad) ->
+    llama_tienda({registra_producto, Producto, Cantidad}).
+
 probar() ->
+    % Abre la tienda
     abre_tienda(),
-    io:format("Abriendo tienda~n"),
-    io:format("Suscribiendo a sebastian~n"),
+    % Suscribe a sebastian
     io:format("~p~n", [llama_tienda({suscribir, sebastian})]),
-    io:format("Listando socios~n"),
+    % Lista los socios
+    io:format("~p~n", [lista_socios()]),
+    % Registrar manzanas
+    io:format("~p~n", [registra_producto(manzanas, 10)]),
+    % Registrar peras
+    io:format("~p~n", [registra_producto(peras, 20)]),
+    % Lista los productos
+    io:format("~p~n", [llama_tienda(lista_productos)]),
+    % Elimina suscripción de sebastian
+    io:format("~p~n", [llama_tienda({eliminar_suscripcion, sebastian})]),
+    % Lista los socios
     io:format("~p~n", [lista_socios()]),
     cierra_tienda().
+
+servidor_producto(Cantidad) ->
+    receive
+        {De, mostrar_disponibilidad} ->
+            De ! {servidor_producto, Cantidad},
+            servidor_producto(Cantidad);
+        {De, {actualizar_disponibilidad, CantidadNueva}} ->
+            De ! {servidor_producto, ok},
+            servidor_producto(CantidadNueva);
+        {De, _} ->
+            De ! {servidor_producto, {error, "Mensaje no reconocido"}},
+            servidor_producto(Cantidad)
+    end.
+
+crear_servidor_producto(Cantidad) ->
+    spawn(servidor_tienda, servidor_producto, [{Cantidad}]).
